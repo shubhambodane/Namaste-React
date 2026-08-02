a769540766ed3494e489a2348a23a617f3b37f1a
Shubham Bodane <shubhambodane@gmail.com>
2026-08-02 11:05:07 +0530

    added corsproxy url in swiggy api

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,6 +4,6 @@ const CDN_URL =
   'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/';
 
 const SWIGGY_RESTAURANT_API =
-  'https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.620047&lng=73.7441082&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING';
+  'https://corsproxy.io/?url=https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.620047&lng=73.7441082&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING';
 
 export { LOGO_URL, CDN_URL, SWIGGY_RESTAURANT_API };
