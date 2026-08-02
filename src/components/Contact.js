5918c6ae59b5a39d988e2f7ec1f328d71dfbc76b
Shubham Bodane <shubhambodane@gmail.com>
2026-08-02 11:52:09 +0530

    contact component

---
+++ src/components/Contact.js
@@ -0,0 +1,10 @@
+const Contact = () => {
+  return (
+    <div>
+      <p> Email : shubhambodane@gmail.com</p>
+      <p> Contact Number : 8484055506</p>
+    </div>
+  );
+};
+
+export default Contact;
