47ce7e328bec4dc3a50d238b40c31d9429d89867
Shubham Bodane <shubhambodane@gmail.com>
2026-08-02 11:50:19 +0530

    about component

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+const About = () => {
+  return (
+    <div>
+      <p>
+        My name is Shubham Bodane, I am working as an full stack developer in a
+        product based company since 7 years.
+      </p>
+    </div>
+  );
+};
+
+export default About;
