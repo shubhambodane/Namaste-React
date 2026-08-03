d3ff6b6d74480d6dbdcb50ee5dd6fd0016586111
Shubham Bodane <shubhambodane@gmail.com>
2026-08-03 19:33:06 +0530

    state variable in class based component

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -3,13 +3,27 @@ import React from 'react';
 class UserClass extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      count: 0,
+      count1: 1,
+    };
   }
   render() {
     const { name, location } = this.props;
-
+    const { count } = this.state;
     return (
       <div className="user-card">
         <h3> Name: {name} </h3>
+        <p> Count: {count}</p>
+        <button
+          onClick={() => {
+            this.setState({ count: count + 1 });
+          }}
+        >
+          {' '}
+          Increase Count
+        </button>
         <h4> Location: {location} </h4>
         <h4> Contact: https://www.github.com/shubhambodane</h4>
       </div>
