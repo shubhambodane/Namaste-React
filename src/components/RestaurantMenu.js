0ae08d260991df2166340041f5812a5e374e85cb
Shubham Bodane <shubhambodane@gmail.com>
2026-08-05 09:08:21 +0530

    moved fetchdata logic to separate file by creating new hook

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,38 +1,13 @@
-import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import Shimmer from './Shimmer';
 import { GET_RESTAURANT_MENU } from '../utils/constants';
+import useRestaurantMenu from '../utils/useRestaurantMenu';
 
 const RestaurantMenu = () => {
   const { resId } = useParams();
-  const [resInfo, setResInfo] = useState(null);
-  const [isError, setIsError] = useState(false);
+  const resInfo = useRestaurantMenu(resId);
 
-  const fetchMenu = async () => {
-    try {
-      const url = GET_RESTAURANT_MENU(resId);
-      const data = await fetch(url);
-      const json = await data.json();
-      setResInfo(json);
-    } catch (error) {
-      console.error('Failed to fetch menu:', error);
-      setIsError(true);
-    }
-  };
-
-  useEffect(() => {
-    fetchMenu();
-  }, [resId]);
-
-  if (isError) {
-    return (
-      <div className="menu">
-        <p>Unable to load restaurant menu. Please try again later.</p>
-      </div>
-    );
-  }
-
-  if (resInfo === null) {
+  if (!resInfo) {
     return <Shimmer />;
   }
 
@@ -40,9 +15,9 @@ const RestaurantMenu = () => {
     name = '',
     cuisines = [],
     costForTwoMessage = '',
-  } = resInfo?.data?.cards?.[2].card?.card?.info;
+  } = resInfo?.cards?.[2].card?.card?.info;
   const { itemCards } =
-    resInfo?.data?.cards?.[4].groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card
+    resInfo?.cards?.[4].groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card
       ?.card;
 
   return (
@@ -54,7 +29,7 @@ const RestaurantMenu = () => {
       <h3>Menu</h3>
       <ul>
         {itemCards.map((item) => (
-          <li>
+          <li key={item.card.info.id}>
             {item.card.info.name} - ₹{item.card.info.price / 100}
           </li>
         ))}
