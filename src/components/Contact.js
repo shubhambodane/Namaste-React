b0f5a78fe73d2e05a589cf22f549f50823261dbf
Shubham Bodane <shubhambodane@gmail.com>
2026-08-15 15:51:58 +0530

    added tests for  contact component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,26 @@
 const Contact = () => {
   return (
     <div>
-      <p> Email : shubhambodane@gmail.com</p>
-      <p> Contact Number : 8484055506</p>
+      <h1 className="font-bold text-3xl p-4 m-4">Contact Us</h1>
+      <input
+        type="text"
+        placeholder="Name"
+        className="border-2 border-gray-300 p-2 m-2 rounded-md"
+      />
+      <input
+        type="email"
+        placeholder="Email"
+        className="border-2 border-gray-300 p-2 m-2 rounded-md"
+      />
+      <textarea
+        placeholder="Message"
+        className="border-2 border-gray-300 p-2 m-2 rounded-md"
+      ></textarea>
+      <button className="bg-blue-500 text-white p-2 m-2 rounded-md">
+        Submit
+      </button>
+      {/* <p> Email : shubhambodane@gmail.com</p>
+      <p> Contact Number : 8484055506</p> */}
     </div>
   );
 };
---
+++ src/components/__tests__/Contact.test.js
@@ -0,0 +1,16 @@
+import { render, screen } from '@testing-library/react';
+import Contact from '../Contact';
+import '@testing-library/jest-dom';
+
+test('Should load contact us component', () => {
+  render(<Contact />);1
+
+  const heading = screen.getByRole('heading');
+  expect(heading).toBeInTheDocument();` `
+});
+
+test('Should load button inside Contact component', () => {
+  render(<Contact />);
+  const button = screen.getByText('Submit');
+  expect(button).toBeInTheDocument();
+});
